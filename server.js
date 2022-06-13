7adf5da18f9ed886a3ee9b885943a95e480c401e
Charles Bartlett <ownageunit@gmail.com>
2022-06-13 11:02:05 -0400

    wip: set up model and routes

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 // Dependencies 
-import express from 'express'
+const express = require('express')
+// import express from 'express'
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const cors = require('cors')
+// import mongoose from 'mongoose'
 
 // Initialize the express app
 const app = express()
@@ -8,17 +13,64 @@ const app = express()
 require('dotenv').config()
 
 // const PORT = process.env.PORT
-const {PORT = 4000, MONGODDB_URL} = proces.env
+const {PORT = 3000, MONGODB_URL} = process.env
+
+// connect to mongoDB
+mongoose.connect(MONGODB_URL)
+
+// mongoose status listeners
+mongoose.connection
+.on('connected', () => console.log('connected to MongoDB'))
+.on('error', (err) => console.log(`error with MongoDB: ${err.message}`));
+
+// setup model
+const peopleSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    title: String
+}, {timestamps: true})
+
+const People = mongoose.model('People', peopleSchema)
 
 // mount middleware 
+app.use(cors()) // Access-Conctol-Allow: '*'
+app.use(morgan('dev'))
+app.use(express.json()) // this creates req.body
+// app.use(express.urlencoded({extended: false})) ONLY WORKS WHEN EXPRESS IS SERVING HTML
 
 // mount routes
 app.get('/', (req, res) => {
     res.send('/ route')
 })
 
-// tell express to listen
+// Index
+app.get('/people', async (req, res) => {
+    try {
+        const people = await People.find({})
+    res.send(people)
+    } catch (error) {
+        console.log(`Error: ${error}`)
+        res.send({error: 'something went wrong - check console'})
+    }
+})
 
+// Create
+app.post('/people', async (req, res) => {
+    try {
+        const person = await People.create(req.body);
+        res.send(person)
+    } catch (error) {
+        console.log(`Error: ${error}`)
+        res.send({error: 'something went wrong - check console'})
+    }
+})
+
+
+// Update
+
+// Delete
+
+// tell express to listen
 app.listen(PORT, () => {
     console.log(`Express is listening on PORT:${PORT}`)
 })
